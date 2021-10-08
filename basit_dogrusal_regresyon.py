636a3a1d03fa8dd4f5448e8b23fb6ff1930c9662
serkancam <sekocam@gmail.com>
2021-10-08 19:34:07 +0300

    t08102021

---
+++ basit_dogrusal_regresyon.py
@@ -0,0 +1,27 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv("veri_setleri/dogrusal_regresyon_veriseti.csv",sep=";")
+print(data.head(5))
+
+plt.scatter(x=data.deneyim,y=data.maas)
+plt.xlabel("deneyim")
+plt.ylabel("maaş")
+plt.show()
+
+lr=LinearRegression()
+X=data.deneyim.values.reshape(-1,1)
+y=data.maas.values.reshape(-1,1)
+lr.fit(X,y)
+
+a=lr.coef_
+b=lr.intercept_
+print(f"a={a},b={b}")
+
+tahmin = lr.predict([[9],[11],[16]])
+print(tahmin)
+
+
